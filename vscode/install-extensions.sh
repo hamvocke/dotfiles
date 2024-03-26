3b26f41479f6d7ff5da3fe217f079215bf034a3c
Ham Vocke <ham@hamvocke.com>
2024-03-26 10:20:15 +0100

    remove vscode config

--- vscode/install-extensions.sh
+++
@@ -1,9 +0,0 @@
-#! /usr/bin/env bash
-
-echo "Installing Visual Studio Code extensions"
-
-for extension in $(cat extensions)
-do
-    echo "Installing: ${extension}"
-    code --install-extension $extension
-done
