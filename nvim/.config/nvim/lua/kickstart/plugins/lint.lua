2d5a422a21faccab3d6a478919701398efac83f7
Ham Vocke <ham@hamvocke.com>
2024-07-09 15:32:23 +0200

    add js/ts linting

--- nvim/.config/nvim/lua/kickstart/plugins/lint.lua
+++ nvim/.config/nvim/lua/kickstart/plugins/lint.lua
@@ -7,6 +7,9 @@ return {
       local lint = require 'lint'
       lint.linters_by_ft = {
         markdown = { 'markdownlint' },
+        javascript = { 'eslint' },
+        typescript = { 'eslint' },
+        json = { 'eslint' },
       }
 
       -- To allow other plugins to add linters to require('lint').linters_by_ft,
