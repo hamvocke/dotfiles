b2864b0cea10ea016dde83c7e1fbb85d09691fb3
Ham Vocke <ham@hamvocke.com>
2024-07-20 18:20:56 +0200

    setup autoformatting for js/ts via prettier

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -571,6 +571,8 @@ require('lazy').setup({
       end,
       formatters_by_ft = {
         lua = { 'stylua' },
+        javascript = { { 'prettierd', 'prettier' } },
+        typescript = { { 'prettierd', 'prettier' } },
         -- Conform can also run multiple formatters sequentially
         -- python = { "isort", "black" },
         --
