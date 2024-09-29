ba5f9f634148a729e066adab6af6d124266d54c0
Ham Vocke <ham@hamvocke.com>
2024-09-29 19:43:28 +0200

    nvim: fix ts-server installation

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -539,7 +539,7 @@ require('lazy').setup({
         --    https://github.com/pmizio/typescript-tools.nvim
         --
         -- But for many setups, the LSP (`tsserver`) will work just fine
-        tsserver = {},
+        ts_ls = {},
         astro = {},
         elixirls = {},
         svelte = {},
