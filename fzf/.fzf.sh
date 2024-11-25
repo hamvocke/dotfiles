3da76bcac61d136737f096c062d3ec4834feec30
Ham Vocke <ham@hamvocke.com>
2024-11-25 19:59:31 +0100

    fzf: switch to root loops theme

--- fzf/.fzf.sh
+++ fzf/.fzf.sh
@@ -1,9 +1,8 @@
 # Set up fzf keybindings, autocompletion, and schmanzy colors
-#
 source <(fzf --zsh)
 
 # Set some spiffy colors (Root Loops style!)
-export FZF_DEFAULT_OPTS=" \
---color=bg+:#101526,bg:#101526,spinner:#ceb5fd,hl:#fb80aa \
---color=fg:#8d9cca,header:#ceb5fd,info:#b5d427,pointer:#ceb5fd \
---color=marker:#fda7c1,fg+:#dde2f2,prompt:#4d5c8a,hl+:#fda7c1"
+export FZF_DEFAULT_OPTS="  --color=fg:#e7ebf6,fg+:#f5f6fb,bg:#1a2137,bg+:#2e3859 \
+--color=hl:#28c8b1,hl+:#30e1c7,info:#f49437,marker:#a1bd22 \
+--color=prompt:#fb80aa,spinner:#bd97fc,pointer:#bd97fc,header:#56b8f7 \
+--color=border:#586899,label:#bcc6e3,query:#e7ebf6"
