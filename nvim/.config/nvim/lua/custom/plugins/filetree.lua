418a2ad501f4eb6163e922e7acbc492ba507018f
Ham Vocke <ham@hamvocke.com>
2025-08-28 09:04:35 +0200

    nvim: auto-close neo-tree if it's the last window

--- nvim/.config/nvim/lua/custom/plugins/filetree.lua
+++ nvim/.config/nvim/lua/custom/plugins/filetree.lua
@@ -1,15 +1,17 @@
 return {
-  "nvim-neo-tree/neo-tree.nvim",
-  version = "*",
+  'nvim-neo-tree/neo-tree.nvim',
+  version = '*',
   dependencies = {
-    "nvim-lua/plenary.nvim",
-    "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
-    "MunifTanjim/nui.nvim",
+    'nvim-lua/plenary.nvim',
+    'nvim-tree/nvim-web-devicons', -- not strictly required, but recommended
+    'MunifTanjim/nui.nvim',
   },
   keys = {
-    { "<leader>l", "<cmd>Neotree toggle<cr>", desc = "[L]ist Files" },
+    { '<leader>l', '<cmd>Neotree toggle<cr>', desc = '[L]ist Files' },
   },
-  config = function ()
-    require('neo-tree').setup {}
+  config = function()
+    require('neo-tree').setup {
+      close_if_last_window = true,
+    }
   end,
 }
