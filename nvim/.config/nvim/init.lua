1674d33aabe2fcc91e621d64b213c0e4dbf204f8
Ham Vocke <ham@hamvocke.com>
2026-01-08 19:39:58 +0100

    nvim: fix treesitter config

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -747,7 +747,7 @@ require('lazy').setup({
   { -- Highlight, edit, and navigate code
     'nvim-treesitter/nvim-treesitter',
     build = ':TSUpdate',
-    main = 'nvim-treesitter.configs', -- Sets main module to use for opts
+    main = 'nvim-treesitter.config', -- Sets main module to use for opts
     -- [[ Configure Treesitter ]] See `:help nvim-treesitter`
     opts = {
       ensure_installed = {
