78f1b1ae7a1113c94ea8a48ac82c53ff1f823476
hamvocke <hermann.vocke@getpliant.com>
2026-04-01 12:01:08 +0200

    nvim: reintroduce highlight on yank

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -1,6 +1,7 @@
 require 'plugins.options'
 require 'plugins.treesitter'
 require 'plugins.completion'
+require 'plugins.autoformat'
 require 'plugins.lsp'
 require 'plugins.picker'
 require 'plugins.whichkey'
@@ -10,3 +11,12 @@ require 'plugins.statusline'
 
 -- auto-update plugins on startup
 -- vim.pack.update()
+
+-- highlight on yank
+vim.api.nvim_create_autocmd('TextYankPost', {
+  callback = function()
+    vim.hl.on_yank {
+      higroup = 'Search',
+    }
+  end,
+})
