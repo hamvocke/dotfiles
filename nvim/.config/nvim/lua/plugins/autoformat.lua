dbc4596138a1b64413eed7aa32a94b1f3d32456c
hamvocke <hermann.vocke@getpliant.com>
2026-04-01 12:02:08 +0200

    nvim: autoformat for rust, html

--- nvim/.config/nvim/lua/plugins/autoformat.lua
+++ nvim/.config/nvim/lua/plugins/autoformat.lua
@@ -1,17 +1,19 @@
--- autoformattinf on save
+-- autoformatting on save
 vim.pack.add({
   'https://github.com/stevearc/conform.nvim',
 }, { confirm = false })
 
 require('conform').setup {
-  notify_on_error = false,
   format_on_save = {
     timeout_ms = 500,
     lsp_format = 'fallback',
   },
+  notify_on_error = true,
   formatters_by_ft = {
     lua = { 'stylua' },
     -- python = { "isort", "black" },
+    html = { 'prettierd', 'prettier', stop_after_first = true },
+    rust = { 'rustfmt' },
     javascript = { 'prettierd', 'prettier', stop_after_first = true },
     typescript = { 'prettierd', 'prettier', stop_after_first = true },
     elixir = { 'mix' },
