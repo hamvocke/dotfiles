036be89c593782353166d825d5890f87e15409a5
Ham Vocke <ham@hamvocke.com>
2026-04-09 11:55:02 +0200

    nvim: fix treesitter config, add new languages

--- nvim/.config/nvim/lua/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/plugins/treesitter.lua
@@ -1,30 +1,44 @@
 -- formatting and syntax highlighting
-vim.pack.add({ 'https://github.com/nvim-treesitter/nvim-treesitter' }, { confirm = false })
+vim.pack.add({ { src = 'https://github.com/nvim-treesitter/nvim-treesitter', version = 'main' } }, { confirm = false })
 
 -- equivalent to :TSUpdate
 require('nvim-treesitter.install').update 'all'
 
 local languages = {
-  'lua',
   'astro',
   'bash',
+  'comment',
+  'diff',
+  'dockerfile',
+  'eex',
   'elixir',
+  'gitcommit',
+  'gitignore',
   'go',
+  'heex',
+  'html',
   'java',
   'javascript',
+  'json',
+  'lua',
   'markdown',
+  'markdown_inline',
+  'nix',
   'svelte',
   'rust',
+  'sql',
   'typescript',
+  'vim',
+  'vimdoc',
+  'yaml',
 }
 
 require('nvim-treesitter').install(languages)
 
 vim.api.nvim_create_autocmd('FileType', {
-  pattern = { '<filetype>' },
   callback = function()
     -- Enable treesitter highlighting and disable regex syntax
-    vim.treesitter.start()
+    pcall(vim.treesitter.start)
 
     -- Enable treesitter-based indentation
     vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
